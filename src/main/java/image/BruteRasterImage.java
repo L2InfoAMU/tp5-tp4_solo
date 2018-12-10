2eea8128bdfd977a36e721c3b8337e9bc32e8c0d
RiikuZen <nicolas.cauet@yahoo.fr>
2018-12-10 11:54:24 +0100

    Fin BruteRasterImage

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -5,41 +5,42 @@ import util.Matrices;
 public  class BruteRasterImage implements Image {
 
     private Color[][] colors;
-    private Color color;
     private int width;
     private int height;
 
     public BruteRasterImage(Color color, int width, int height) {
-        this.color = color;
         this.width = width;
         this.height = height;
+
+        createRepresentation();
+        setPixelsColor(color);
     }
 
     public BruteRasterImage(Color[][] colors) {
         Matrices.requiresNonZeroDimensions(colors);
         Matrices.requiresNonNull(colors);
+        setWidth(colors.length);
+        setHeight(colors[0].length);
+        createRepresentation();
         this.colors = colors;
     }
 
     public void createRepresentation() {
-        colors = new Color[getHeight()][getWidth()];
-        setPixelsColor(color);
+        colors = new Color[getWidth()][getHeight()];
     }
 
     public void setPixelColor(Color color, int x, int y) {
-        this.color = color;
-        this.width = x;
-        this.height = y;
+        this.colors[x][y] = color;
     }
 
-    public Color getPixelColor(int x, int y) {return colors[y][x];}
+    public Color getPixelColor(int x, int y) {return colors[x][y];}
 
     private void setPixelsColor(Color[][] pixels) {this.colors = pixels;}
 
     private void setPixelsColor(Color color) {
         for (int y = 0; y < getWidth(); y++)
             for (int x = 0; x < getHeight(); x++)
-                setPixelColor(color, y, x);
+                setPixelColor(color, x, y);
     }
 
     public int getWidth() {return this.width;}
